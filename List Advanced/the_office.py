0a11c27bfa35bae3226777eb7c26d5522bd6aad0
seva_Desktop <vsevolodachev@gmail.com>
2026-02-05 15:30:08 +0200

    refactor: rename variable

--- List Advanced/the_office.py
+++ List Advanced/the_office.py
@@ -1,6 +1,6 @@
 input_score = list(map(int, input().split()))
-happines_factor = int(input())
-multiplied_values = [x * happines_factor for x in input_score]
+happiness_factor = int(input())
+multiplied_values = [x * happiness_factor for x in input_score]
 avg_multiplied_values = sum(multiplied_values) / len(input_score)
 happy_employee = sum(num >= avg_multiplied_values for num in multiplied_values)
 
