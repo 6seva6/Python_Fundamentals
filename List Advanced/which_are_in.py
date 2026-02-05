57c65003fedd59a0ed0fff67d8e87a08c4368d24
seva_Desktop <vsevolodachev@gmail.com>
2026-02-05 17:28:48 +0200

    feat: rewrite the solution using list comprehension

--- List Advanced/which_are_in.py
+++ List Advanced/which_are_in.py
@@ -1,11 +1,14 @@
 substr_list = [x for x in input().split(", ")]
 str_list = [x for x in input().split(", ")]
-def are_in(compare, instance):
-    result = []
-    for i in compare:
-        for j in range(len(instance)):
-            if i in str_list[j]:
-                result.append(i)
-    return result
-answer = sorted((list(set(are_in(compare=substr_list, instance=str_list)))), key = lambda x: x)
-print(answer)
+sorted_list = sorted(sub for sub in substr_list if any(sub in list for list in str_list ))
+print(sorted_list)
+
+# def are_in(compare, instance):
+#     result = []
+#     for i in compare:
+#         for j in range(len(instance)):
+#             if i in str_list[j]:
+#                 result.append(i)
+#     return result
+# answer = sorted((list(set(are_in(compare=substr_list, instance=str_list)))), key = lambda x: x)
+# print(answer)
